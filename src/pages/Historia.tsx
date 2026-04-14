d0f4c3aba387c6ff3de289389773dd93c93eaac2
Milan <milanalba@outlook.com>
2026-04-13 19:00:27 -0600

    220px img

--- src/pages/Historia.tsx
+++ src/pages/Historia.tsx
@@ -35,11 +35,11 @@ const Historia = () => {
                         <p className="text-[1.25rem] text-gray-700 leading-relaxed lg:w-7/12">
                             {history_text[0]}
                         </p>
-                        <img src={panoramica} alt="Panorámica" className="w-full lg:w-5/12 h-[280px] rounded-xl shadow-md object-cover" />
+                        <img src={panoramica} alt="Panorámica" className="w-full lg:w-5/12 h-[220px] rounded-xl shadow-md object-cover" />
                     </section>
 
                     <section className="flex flex-col lg:flex-row items-center gap-6 text-left px-4 lg:px-0">
-                        <img src={pasillo} alt="Pasillo" className="w-full lg:w-5/12 h-[280px] rounded-xl shadow-md object-cover" />
+                        <img src={pasillo} alt="Pasillo" className="w-full lg:w-5/12 h-[220px] rounded-xl shadow-md object-cover" />
                         <p className="text-[1.25rem] text-gray-700 leading-relaxed lg:w-7/12">
                             {history_text[1]}
                         </p>
@@ -49,11 +49,11 @@ const Historia = () => {
                         <p className="text-[1.25rem] text-gray-700 leading-relaxed lg:w-7/12">
                             {history_text[2]}
                         </p>
-                        <img src={actividades} alt="Actividades" className="w-full lg:w-5/12 h-[280px] rounded-xl shadow-md object-cover" />
+                        <img src={actividades} alt="Actividades" className="w-full lg:w-5/12 h-[220px] rounded-xl shadow-md object-cover" />
                     </section>
 
                     <section className="flex flex-col lg:flex-row items-center gap-6 text-left px-4 lg:px-0">
-                        <img src={arbol} alt="Árbol" className="w-full lg:w-5/12 h-[280px] rounded-xl shadow-md object-cover" />
+                        <img src={arbol} alt="Árbol" className="w-full lg:w-5/12 h-[220px] rounded-xl shadow-md object-cover" />
                         <p className="text-[1.25rem] text-gray-700 leading-relaxed lg:w-7/12">
                             {history_text[3]}
                         </p>
